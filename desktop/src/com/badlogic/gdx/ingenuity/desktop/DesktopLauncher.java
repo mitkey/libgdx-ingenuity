ad6dd6debd5ab86b99692d363f4fff12585e7b2b
Mitkey <mitkey@foxmail.com>
2016-09-07 18:37:16 +0800

    init gdx project
    Signed-off-by: Mitkey <mitkey@foxmail.com>

---
+++ desktop/src/com/badlogic/gdx/ingenuity/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.badlogic.gdx.ingenuity.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.ingenuity.IngenuityGdx;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new IngenuityGdx(), config);
+	}
+}
