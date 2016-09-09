afb2103164283d5f1f611cf57e3a7250ea99cd81
Mitkey <mitkey@foxmail.com>
2016-09-09 14:06:35 +0800

    监控信息抽取到 GeneralScreen.java
    Signed-off-by: Mitkey <mitkey@foxmail.com>

--- core/src/com/badlogic/gdx/ingenuity/IngenuityScreen.java
+++ core/src/com/badlogic/gdx/ingenuity/IngenuityScreen.java
@@ -3,16 +3,10 @@ package com.badlogic.gdx.ingenuity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.ingenuity.utils.LazyBitmapFont;
 import com.badlogic.gdx.ingenuity.utils.MoveListener;
 import com.badlogic.gdx.ingenuity.utils.scene2d.GeneralScreen;
-import com.badlogic.gdx.ingenuity.utils.scene2d.NumberLabel;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 /**
  * @作者 Mitkey
@@ -22,52 +16,17 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
  */
 public class IngenuityScreen extends GeneralScreen {
 
-	BitmapFont font;
 	Texture texture;
 
 	@Override
 	public void show() {
 		super.show();
 
-		font = new LazyBitmapFont(20);
-		LabelStyle labelStyle = new LabelStyle(font, Color.WHITE);
-
-		NumberLabel<Integer> labFps = new NumberLabel<Integer>("FPS: ", -99, labelStyle) {
-			@Override
-			public Integer getValue() {
-				return Gdx.graphics.getFramesPerSecond();
-			}
-		};
-		NumberLabel<Float> labHeap = new NumberLabel<Float>("Heap: ", -99f, labelStyle) {
-			@Override
-			public Float getValue() {
-				return Gdx.app.getJavaHeap() * 1f / 1024 / 1024;
-			}
-		};
-		NumberLabel<Float> labNative = new NumberLabel<Float>("Native: ", -99f, labelStyle) {
-			@Override
-			public Float getValue() {
-				return Gdx.app.getNativeHeap() * 1f / 1024 / 1024;
-			}
-		};
-
-		Table table = new Table();
-		table.defaults().width(150).left();
-		table.add(labFps).left().row();
-		table.add(labHeap).row();
-		table.add(labNative).row();
-		table.pack();
-		table.layout();
-		table.setPosition(50, 100);
-		stage().addActor(table);
-		table.clipBegin();
-		table.clipEnd();
-
 		texture = new Texture("badlogic.jpg");
 		Image image = new Image(texture);
 		image.setPosition(250, 150);
-		stage().addActor(image);
 		image.addListener(new MoveListener(image));
+		stage().addActor(image);
 
 		InputProcessor processor = new InputMultiplexer(stage());
 		Gdx.input.setInputProcessor(processor);
@@ -89,7 +48,6 @@ public class IngenuityScreen extends GeneralScreen {
 	public void dispose() {
 		// TODO Auto-generated method stub
 		super.dispose();
-		font.dispose();
 		texture.dispose();
 	}
 
--- core/src/com/badlogic/gdx/ingenuity/utils/scene2d/GeneralScreen.java
+++ core/src/com/badlogic/gdx/ingenuity/utils/scene2d/GeneralScreen.java
@@ -2,8 +2,13 @@ package com.badlogic.gdx.ingenuity.utils.scene2d;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.ingenuity.utils.LazyBitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 /**
@@ -18,11 +23,50 @@ public abstract class GeneralScreen extends ScreenAdapter {
 	public static int GameHeight = 720;
 
 	Stage stage;
+	BitmapFont font;
 
 	@Override
 	public void show() {
 		super.show();
 		this.stage = new Stage(new StretchViewport(GameWidth, GameHeight));
+
+		initMonitor();
+
+	}
+
+	private void initMonitor() {
+		this.font = new LazyBitmapFont(18);
+		LabelStyle labelStyle = new LabelStyle(font, Color.WHITE);
+
+		NumberLabel<Integer> labFps = new NumberLabel<Integer>("Fps: ", -1, labelStyle) {
+			@Override
+			public Integer getValue() {
+				return Gdx.graphics.getFramesPerSecond();
+			}
+		};
+		NumberLabel<Float> labHeap = new NumberLabel<Float>("Heap: ", -1f, labelStyle) {
+			@Override
+			public Float getValue() {
+				return Gdx.app.getJavaHeap() * 1f / 1024 / 1024;
+			}
+		};
+		NumberLabel<Float> labNative = new NumberLabel<Float>("Native: ", -1f, labelStyle) {
+			@Override
+			public Float getValue() {
+				return Gdx.app.getNativeHeap() * 1f / 1024 / 1024;
+			}
+		};
+
+		Table table = new Table();
+		table.defaults().width(110).left().pad(5);
+		table.add(labFps).row();
+		table.add(labHeap).row();
+		table.add(labNative).row();
+		table.setPosition(10, 10);
+		table.pack();
+		table.layout();
+		stage.addActor(table);
+		table.toFront();
 	}
 
 	@Override
@@ -47,6 +91,7 @@ public abstract class GeneralScreen extends ScreenAdapter {
 	public void dispose() {
 		super.dispose();
 		stage.dispose();
+		font.dispose();
 	}
 
 	public final Stage stage() {
