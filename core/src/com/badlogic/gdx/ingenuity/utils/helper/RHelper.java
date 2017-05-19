07788bcaebf91dfed1cfef265c979e349c5b1e33
mitkey <mitkey@foxmail.com>
2017-05-19 12:31:11 +0800

    modify and reconsitution RHelper.java
    Signed-off-by: mitkey <mitkey@foxmail.com>

--- core/src/com/badlogic/gdx/ingenuity/utils/helper/RHelper.java
+++ core/src/com/badlogic/gdx/ingenuity/utils/helper/RHelper.java
@@ -1,145 +1,184 @@
 
 package com.badlogic.gdx.ingenuity.utils.helper;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
-import com.badlogic.gdx.Application.ApplicationType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.ingenuity.GdxGame;
-import com.badlogic.gdx.ingenuity.utils.common.StrUtil;
 
+/**
+ * @作者 mitkey
+ * @时间 2017年5月19日 下午12:30:41
+ * @类说明 RHelper.java <br/>
+ * @版本 0.0.1
+ */
 public class RHelper {
+
 	private static final String TAG = RHelper.class.getSimpleName();
-	private static final String ResRoot = "assets";
-	private static HashMap<String, String> resMap = new HashMap<String, String>();
+
+	// 资源文件 R 映射类名
+	private static final String R_JAVA_NAME = "GdxR";
+
+	// 包名格式化字符
+	private static final String STR_FORMAT_PACKAGE = "package %s;\n\n";
+
+	// 类声明格式化字符
+	private static final String STR_FORMAT_CLASS = "public static final class %s {\n\n";
+
+	// 资源映射字段格式化字符
+	private static final String STR_FORMAT_FIELD = "public static final String %s = \"%s\";\n";
 
 	/** 自动读写资源路径 */
 	public static void generated() {
-		// 加载 assets 资源下所有文件的 name 和 path。
-		loadResNames("");
-
-		// 读取模板
-		String rPath = RHelper.class.getName().replaceAll("\\.", "/").replace(RHelper.class.getSimpleName(), "") + "R.template";
-		FileHandle resFileHandle = Gdx.files.classpath(rPath);
-		String line = null;
-		StringBuffer contentBuffer = new StringBuffer();
-		try (BufferedReader bufferedReader = new BufferedReader(resFileHandle.reader())) {
-			while ((line = bufferedReader.readLine()) != null) {
-				if (line.contains("$")) {
-					// 替换变量
-					String demo = line.replace("$", "");
-					for (Map.Entry<String, String> entry : resMap.entrySet()) {
-						String a = String.format(demo, entry.getKey(), entry.getValue()) + "\n";
-						contentBuffer.append(a);
-					}
-				} else if (line.contains("#")) {
-					// 替换包名
-					line = String.format(line.replace("#", ""), getGameClassPath());
-					line = line.replaceAll("/", ".");
-					contentBuffer.append(line + "\n");
-				} else {
-					contentBuffer.append(line + "\n");
-				}
-			}
-		} catch (IOException e) {
-			Gdx.app.error(TAG, "读取操作模板文件错误", e);
+		if (checkCanCreateGdxR()) {
+			String content = generateRjavaContent();
+			writeRjava(content);
+			Gdx.app.debug(TAG, "generated GdxR.java finished");
 		}
-
-		if (contentBuffer.length() > 0) {
-			// 工程名称
-			String projectname = System.getProperty("user.dir").replace("desktop", "core");
-			// =/Users/haocao/git/CHGame/my-gdx-game-core
-			projectname = projectname.substring(projectname.lastIndexOf("/") + 1);
-			// 在相应包目录下创建类文件
-			String coreProjectSrc = String.format("%s/src/", projectname);
-			String resJavaFile = coreProjectSrc + getGameClassPath();
-			File resFile = new File(resJavaFile);
-			if (!resFile.exists()) {
-				resFile.mkdir();
-			}
-			FileHandle fileHandle = new FileHandle(resFile + "/GdxR.java");
-			fileHandle.writeBytes(contentBuffer.toString().getBytes(), false);
-
-			Gdx.app.debug(TAG, "write GdxR.java --> " + fileHandle);
-		}
-
-		Gdx.app.debug(TAG, "generated GdxR.java finished");
 	}
 
-	/** 读取assets目录下全部资源 */
-	private static void loadResNames(String path) {
-		// 处理 path 为 C:\development\git-repository\company-project\libgdx-ingenuity\android\assets 这种路径，转换为 assets 目录下相对 path
-		int idxAssetEnd = path.contains(ResRoot) ? (path.lastIndexOf(ResRoot) + ResRoot.length() + 1) : -1;
-		if (idxAssetEnd > 6) {
-			path = path.substring(idxAssetEnd);
+	/** 检测是否能且有必要生成 GdxR.java 文件 */
+	private static boolean checkCanCreateGdxR() {
+		try {
+			String protocol = RHelper.class.getResource("").getProtocol();
+			if ("file".equals(protocol)) {
+				return true;
+			}
+		} catch (Exception e) {
+			Gdx.app.error(TAG, "check can create GdxR.java fail", e);
 		}
-		// 获取该相对 path 下的所有 file handle
-		FileHandle[] fileHandles = getFileHandles(path);
-		for (FileHandle fileHandle : fileHandles) {
-			String pathAbsolute = fileHandle.path();
-			if (fileHandle.isDirectory()) { // 是目录，继续遍历
-				loadResNames(pathAbsolute);
-			} else {// 是文件
-				// 处理文件名中包含 【.】 的字符替换为 【_】
-				String fileName = fileHandle.name().replaceAll("[\\.\\-]", "_");
-				// 若是 mac 下生成的 .DS_Store 文件则跳过
-				if (fileName.contains("DS_Store")) {
+		return false;
+	}
+
+	private static String generateRjavaContent() {
+		StringBuffer stringBuffer = new StringBuffer();
+		// 包名
+		stringBuffer.append(String.format(STR_FORMAT_PACKAGE, GdxGame.class.getPackage().getName()));
+		// 类声明
+		stringBuffer.append(String.format(STR_FORMAT_CLASS, R_JAVA_NAME));
+
+		// 加载所有的资源名
+		Map<String, List<AssetsNames>> result = loadAssetsDirectory(true, new LinkedHashMap<String, List<AssetsNames>>(), parseAssetsRootPath());
+		Iterator<Entry<String, List<AssetsNames>>> iterator = result.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Entry<String, List<AssetsNames>> entry = iterator.next();
+			for (AssetsNames element : entry.getValue()) {
+				stringBuffer.append("\t").append(String.format(STR_FORMAT_FIELD, element.name, element.filePath));
+			}
+			if (entry.getValue().size() > 0) {
+				stringBuffer.append("\n");
+			}
+		}
+
+		// 类结束
+		stringBuffer.append("}");
+		return stringBuffer.toString();
+	}
+
+	private static Map<String, List<AssetsNames>> loadAssetsDirectory(boolean isRoot, Map<String, List<AssetsNames>> result, String path) {
+		File file = new File(path);
+		// 该路径不存在
+		if (!file.exists()) {
+			Gdx.app.error(TAG, "loadAssetsNames fail, file no exists:" + file.getAbsolutePath());
+			return result;
+		}
+		// 不是目录
+		if (!file.isDirectory()) {
+			Gdx.app.error(TAG, "loadAssetsNames fail, file no directory:" + file.getAbsolutePath());
+			return result;
+		}
+
+		List<AssetsNames> list = new LinkedList<AssetsNames>();
+		// 文件夹名
+		String strFolderName = isRoot ? "def" : path.substring(path.lastIndexOf("\\") + 1);
+		for (File temp : file.listFiles()) {
+			if (temp.isHidden()) {
+				// 隐藏文件跳过
+				continue;
+			}
+			if (temp.isDirectory()) {
+				// 文件夹则递归
+				loadAssetsDirectory(false, result, temp.getAbsolutePath());
+			} else {
+				// 文件
+				String fileName = temp.getName();
+				if (fileName.equals(".DS_Store")) {
 					continue;
 				}
+				// 文件名中【.】 和 【-】 字符替换为 【_】
+				fileName = fileName.replaceAll("[\\.\\-]", "_");
 				// 数字打头的文件，需要处理下
 				if (Character.isDigit(fileName.toCharArray()[0])) {
 					fileName = "_" + fileName;
 				}
 
-				// 路径转换为 assets 目录下的相对 path
-				int assetEnd = pathAbsolute.lastIndexOf(ResRoot);
-
-				// 文件夹路径基于 assets
-				String folderPath = pathAbsolute.substring(assetEnd + ResRoot.length() + 1, pathAbsolute.indexOf(fileHandle.name()));
-				folderPath = folderPath.replaceAll("/", "");
-
-				String key = folderPath + (StrUtil.isBlank(folderPath) ? "" : "_") + fileName;
-				String value = pathAbsolute.substring(assetEnd + ResRoot.length() + 1);
-				resMap.put(key, value);
+				// 资源对应的字段名
+				String name = strFolderName + "_" + fileName;
+				// E:\git-repository\libgdx-ingenuity\android\assets\asdasd\xxx.txt 替换为 asdasd\xxx.txt
+				String filePath = temp.getAbsolutePath().replace(parseAssetsRootPath() + "\\", "");
+				filePath = filePath.replaceAll("\\\\", "/");
+				list.add(new AssetsNames(name, filePath));
 			}
+		}
+		result.put(strFolderName, list);
+		return result;
+	}
+
+	private static String parseAssetsRootPath() {
+		// core 中启动调用 E:\git-repository\libgdx-ingenuity\core
+		// desktop 中启动调用 E:\git-repository\libgdx-ingenuity\desktop
+		String absolutePath = new File("").getAbsolutePath();
+		// E:\git-repository\libgdx-ingenuity
+		absolutePath = absolutePath.substring(0, absolutePath.lastIndexOf("\\"));
+		// E:\git-repository\libgdx-ingenuity\android\assets
+		absolutePath = absolutePath + "/android/assets";
+		return absolutePath;
+	}
+
+	private static String parseGdxRjavaPath() {
+		// core 中启动调用 E:\git-repository\libgdx-ingenuity\core
+		// desktop 中启动调用 E:\git-repository\libgdx-ingenuity\desktop
+		String srcPath = new File("").getAbsolutePath();
+		// E:\git-repository\libgdx-ingenuity
+		srcPath = srcPath.substring(0, srcPath.lastIndexOf("\\"));
+		// E:\git-repository\libgdx-ingenuity\core\src
+		srcPath = srcPath + "/core/src";
+		// E:/git-repository/libgdx-ingenuity/core/src
+		srcPath = srcPath.replaceAll("\\\\", "/");
+
+		// com.badlogic.gdx.ingenuity 转换为 com/badlogic/gdx/ingenuity
+		String packPath = GdxGame.class.getPackage().getName().replaceAll("\\.", "/");
+		// E:/git-repository/libgdx-ingenuity/core/src/com/badlogic/gdx/ingenuity/GdxR.java
+		return srcPath + "/" + packPath + "/" + R_JAVA_NAME + ".java";
+	}
+
+	private static void writeRjava(String content) {
+		// 写入
+		FileHandle fileHandle = new FileHandle(new File(parseGdxRjavaPath()));
+		if (fileHandle.exists() && content.equals(fileHandle.readString())) {
+			Gdx.app.debug(TAG, "write cancel(content the same) " + R_JAVA_NAME + " --> " + fileHandle);
+		} else {
+			fileHandle.writeString(content, false, "UTF-8");
+			Gdx.app.debug(TAG, "write success " + R_JAVA_NAME + " --> " + fileHandle);
 		}
 	}
 
-	/** 读取文件下所有文件 */
-	private static FileHandle[] getFileHandles(String fileName) {
-		if (Gdx.app.getType() == ApplicationType.Desktop) {
-			try {
-				File directory = new File("");// 参数为空
-				String courseFile = directory.getCanonicalPath();
-				courseFile = courseFile.replace("desktop", "android");
-				File assetFile = new File(courseFile + File.separator + ResRoot + File.separator + fileName);
-				File[] files = assetFile.listFiles();
-				if (files == null)
-					return new FileHandle[] {};
-				FileHandle[] handles = new FileHandle[files.length];
-				for (int i = 0, l = files.length; i < l; i++) {
-					handles[i] = new FileHandle(files[i]);
-				}
-				return handles;
-			} catch (IOException ioException) {
-				Gdx.app.error(TAG, "load assets error :" + ioException.getLocalizedMessage());
-			}
+	private static class AssetsNames {
+		public String name;
+		public String filePath;
+
+		public AssetsNames(String name, String filePath) {
+			super();
+			this.name = name;
+			this.filePath = filePath;
 		}
-
-		FileHandle parentFileHandle = Gdx.files.internal(fileName);
-		return parentFileHandle.list();
 	}
 
-	/** 获取 game 类的继续类所在包路径 */
-	private static String getGameClassPath() {
-		Class<?> gameclass = GdxGame.class;
-		String gameClassPath = gameclass.getName().replaceAll("\\.", "/");
-		gameClassPath = gameClassPath.substring(0, gameClassPath.lastIndexOf("/"));
-		return gameClassPath;
-	}
 }
