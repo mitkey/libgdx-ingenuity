65482bdc6c357d98c514e0cd27817194421ee2df
mitkey <mitkey@foxmail.com>
2017-06-02 16:49:43 +0800

    add test project
    Signed-off-by: mitkey <mitkey@foxmail.com>

---
+++ test/desktop/src/com/badlogic/gdx/ingenuity/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.badlogic.gdx.ingenuity.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.ingenuity.GdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxGame(), config);
+	}
+}
