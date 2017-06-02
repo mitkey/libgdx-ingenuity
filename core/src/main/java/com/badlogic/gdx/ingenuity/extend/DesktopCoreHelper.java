5b9e1ffa204dcf6458e00ff8595bb6ba8f7d19e1
mitkey <mitkey@foxmail.com>
2017-06-02 17:27:23 +0800

    update

---
+++ core/src/main/java/com/badlogic/gdx/ingenuity/extend/DesktopCoreHelper.java
@@ -0,0 +1,49 @@
+package com.badlogic.gdx.ingenuity.extend;
+
+import java.util.UUID;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ingenuity.utils.common.StrUtil;
+import com.badlogic.gdx.ingenuity.utils.config.Storage;
+
+/**
+ * @作者 Mitkey
+ * @时间 2017年3月24日 下午2:27:15
+ * @类说明:
+ * @版本 xx
+ */
+public class DesktopCoreHelper implements ICoreHelper {
+
+	// 存储 pc 端唯一设备码的 key
+	private static final String DEVICE_KEY = "deviceId";
+
+	@Override
+	public String getVersion() {
+		return "0.1.1";
+	}
+
+	@Override
+	public String getDeviceUniqueId() {
+		String curStr = Storage.getStr(DEVICE_KEY, null);
+		if (StrUtil.isNotBlank(curStr)) {
+			return curStr;
+		} else {
+			curStr = UUID.randomUUID().toString();
+			Storage.saveStr(DEVICE_KEY, curStr);
+			return curStr;
+		}
+	}
+
+	@Override
+	public String getDeviceType() {
+		return "PC";
+	}
+
+	@Override
+	public void existApp() {
+		Gdx.app.exit();
+		System.exit(0);
+		System.gc();
+	}
+
+}
